a8966bb080570c81529dd0a749e3f1aaaaaace0c
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-09 13:56:46 -0300

    Set up factories for categories and posts

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :category do
     name { "MyString" }
-    branch { "MyString" }
+    branch { "MyString123" }
   end
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :post do
-    
+    title {"Title test"}
+    content {"title test content to factories post"}
+    category
+    user
   end
 end
