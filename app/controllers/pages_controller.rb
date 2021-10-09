f60fb22db629356f37cd1f9459580e1024aca961
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-09 19:00:40 -0300

    Generate posts routes, generated posts controller to retrieve posts, created the index on the pages controller for homepage, created partial for posts on posts folder vie to be called on the pages controller rendering partial post on the index.html.erb for pages main content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, :only => "index", :raise => false
   def index
+    @posts = Post.limit(5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
+  resources :posts do
+    get 'hobby'
+    get 'study'
+    get 'team'
+  end
 end
