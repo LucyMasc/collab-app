7a72b2b38862c867e7ccc3532ef0bf70868b4333
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-09 09:57:24 -0300

    Rspec feature for login session devise

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,16 +1,18 @@
 require "rails_helper"
-require "factory_bot"
+# require "factory_bot"
 
 RSpec.describe 'Login', type: :feature do
   # You should be using pure Capybara helpers here
-  scenario 'user navigates to the login page and succesfully logs in' do
+
+  scenario 'user navigates to the login page and succesfully logs in', js: true do
+    # starting from the homepage
     user = FactoryBot.create(:user)
     visit root_path
-    # find('#login', visible: false).click
     click_link 'Login'
-    # find('#login', visible: false).click
+    # find('#login', visible: false).click is also accepted above
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
+    # find below same goal of the click_link 'Login', but fin is for css selectors
     find('#login2', visible: false).click
     # expect(page).to have_current_path(root_path)
     expect(page).to have_text 'Signed in successfully'
