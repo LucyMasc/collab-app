0095bf0539e9259c0031c24dfda01e0c635a5ff3
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-09 21:17:12 -0300

    Modal need to be fixed

---
+++ app/javascript/posts/modal.js
@@ -0,0 +1,17 @@
+// document.addEventListener("turbolinks:load", function () {
+//   document.querySelector("body").addEventListener("click");
+// });
+
+$(document).on("turbolinks:load", function () {
+  $("body").on("click", ".single-post-card, .single-post-list", function () {
+    var posted_by = $(this).find(".post-content .posted-by").html();
+    var post_heading = $(this).find(".post-content h3").html();
+    var post_content = $(this).find(".post-content p").html();
+    var interested = $(this).find(".post-content .interested").attr("href");
+    $(".modal-header .posted-by").text(posted_by);
+    $(".loaded-data h3").text(post_heading);
+    $(".loaded-data p").text(post_content);
+    $(".loaded-data .interested a").attr("href", interested);
+    $(".myModal").modal("show");
+  });
+});
